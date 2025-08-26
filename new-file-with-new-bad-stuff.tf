040e76d721f364788cdf1f19134a7ea8fb37b79c
Sam <sam.mirzaee@plerion.com>
2025-08-26 16:15:47 +1000

    infra: add more bad stuff

---
+++ new-file-with-new-bad-stuff.tf
@@ -0,0 +1,13 @@
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = "nAH2VzKrMrRjySLlt8HCdFU3tM2TUuUZgh39NX"
+}
+
+resource "aws_s3_bucket" "a_very_bad_public_s3_bucket" {
+  bucket = "my-public-bucket-3"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket" "a_second_bad_public_s3_bucket" {
+  bucket = "my-public-bucket-8"
+  acl    = "public-read"
+}
